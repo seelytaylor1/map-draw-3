bf35518066805151bfa05c6871dbfaacf76cdc7a
Seely Taylor <seelytaylor@gmail.com>
2026-06-12 18:43:21 -0300

    refactor: remove ISO_FRONT_FACE_COLOR and ISO_EAST_FACE_COLOR constants

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,5 @@ export type TileState = typeof WALL | typeof FLOOR | typeof WATER
 export const FLOOR_COLOR = '#f5f0e8'
 export const WATER_COLOR = '#6baed6'
 export const FACE_COLOR = '#6a5040'
-export const ISO_FRONT_FACE_COLOR = '#5a4030'
-export const ISO_EAST_FACE_COLOR = '#7a5a44'
 
 export const Z_STEP_HEIGHT = TILE_PX / 2 + FACE_PX  // 18px at current constants
